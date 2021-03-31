ea2b8577e1c6d1a5162277b80d6c1f75051b2c9f
heeyoung-choi <75930620+heeyoung-choi@users.noreply.github.com>
2021-03-31 21:10:57 +0700

    Add files via upload

---
+++ cs162_temporary/Project_temporary/Project_temporary/main.cpp
@@ -0,0 +1,25 @@
+#include"handling.h"
+
+staffNode* headStaff;
+studentNode* headStudent;
+
+/*void addStaffFromCSV(string filename, string role)
+{
+	ifstream f;
+	f.open(filename);
+	string str;
+	f >> str;
+	if (str[0] == 'T')
+	{
+		studentNode* newStu = new studentNode;
+
+	}
+	else if (str[0] == 'F')
+	f.close();
+}*/
+
+int main()
+{
+
+	return 0;
+}
