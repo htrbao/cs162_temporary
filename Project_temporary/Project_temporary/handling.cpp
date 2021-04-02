2b6dbf12540762121bf5da04f9ac82315a051de2
htrbao <trbaohoo@gmail.com>
2021-04-02 20:53:07 +0700

    edited

--- Project_temporary/Project_temporary/handling.cpp
+++ Project_temporary/Project_temporary/handling.cpp
@@ -1,4 +1,5 @@
 #include "handling.h"
+
 void addStudentNode(studentNode*& head, studentNode*& end, studentNode* pItem)
 {
 	if (!head)
@@ -10,6 +11,7 @@ void addStudentNode(studentNode*& head, studentNode*& end, studentNode* pItem)
 	end->pNext = pItem;
 	end = end->pNext;
 }
+
 void addStaffNode(staffNode*& head, staffNode*& end, staffNode* pItem)
 {
 	if (!head)
@@ -20,4 +22,29 @@ void addStaffNode(staffNode*& head, staffNode*& end, staffNode* pItem)
 	}
 	end->pNext = pItem;
 	end = end->pNext;
+}
+
+void addDataToStaffCsvFile(ofstream fout, staffNode* pHead)
+{
+	staffNode* pCur = pHead;
+	fout.open("STAFF.txt", ios::app);
+	fout << "name,id,username,password";
+	while (pCur != nullptr) {
+		fout << pCur->name << "," << pCur->id << "," << pCur->username << "," << pCur->password;
+		pCur = pCur->pNext;
+	}
+
+	fout.close();
+}
+
+void addDataToStudentCsvFile(ofstream fout, studentNode* pHead) {
+	studentNode* pCur = pHead;
+	fout.open("STUDENT.txt", ios::app);
+	fout << "name,id,username,password";
+	while (pCur != nullptr) {
+		fout << pCur->name << "," << pCur->id << "," << pCur->username << "," << pCur->password;
+		pCur = pCur->pNext;
+	}
+
+	fout.close();
 }
--- Project_temporary/Project_temporary/handling.h
+++ Project_temporary/Project_temporary/handling.h
@@ -28,41 +28,9 @@ struct staffNode {
 	staffNode* pNext;
 };
 
-#endif // !_HANDLING_H_
-void addStaffNode(staffNode*& head, staffNode*& end, staffNode* pItem)
-{
-	if (!head)
-	{
-		head = pItem;
-		end = pItem;
-		return;
-	}
-	end->pNext = pItem;
-	end = end->pNext;
-}
-
-void addDataToStaffCsvFile(ofstream fout, staffNode* pHead) {
-	staffNode* pCur = pHead;
-	fout.open("STAFF.txt", ios::app);
-	fout << "name,id,username,password";
-	while (pCur != nullptr) {
-		fout << pCur->name << "," << pCur->id << "," << pCur->username << "," << pCur->password;
-		pCur = pCur->pNext;
-	}
-
-	fout.close();
-}
-
-void addDataToStudentCsvFile(ofstream fout, studentNode* pHead) {
-	studentNode* pCur = pHead;
-	fout.open("STUDENT.txt", ios::app);
-	fout << "name,id,username,password";
-	while (pCur != nullptr) {
-		fout << pCur->name << "," << pCur->id << "," << pCur->username << "," << pCur->password;
-		pCur = pCur->pNext;
-	}
-
-	fout.close();
-}
 void addStudentNode(studentNode*& head, studentNode*& end, studentNode* pItem);
 void addStaffNode(staffNode*& head, staffNode*& end, staffNode* pItem);
+void addDataToStaffCsvFile(ofstream fout, staffNode* pHead);
+void addDataToStudentCsvFile(ofstream fout, studentNode* pHead);
+
+#endif // !_HANDLING_H_
