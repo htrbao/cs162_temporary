59e0bd996b441128800efb95f9f7dd9d8e16479f
NhanHcmus20 <81221996+NhanHcmus20@users.noreply.github.com>
2021-04-02 13:47:22 +0700

    Function addDataToCsvFile

--- Project_temporary/Project_temporary/handling.h
+++ Project_temporary/Project_temporary/handling.h
@@ -40,5 +40,29 @@ void addStaffNode(staffNode*& head, staffNode*& end, staffNode* pItem)
 	end->pNext = pItem;
 	end = end->pNext;
 }
+
+void addDataToStaffCsvFile(ofstream fout, staffNode* pHead) {
+	staffNode* pCur = pHead;
+	fout.open("STAFF.txt", ios::app);
+	fout << "name,id,username,password";
+	while (pCur != nullptr) {
+		fout << pCur->name << "," << pCur->id << "," << pCur->username << "," << pCur->password;
+		pCur = pCur->pNext;
+	}
+
+	fout.close();
+}
+
+void addDataToStudentCsvFile(ofstream fout, studentNode* pHead) {
+	studentNode* pCur = pHead;
+	fout.open("STUDENT.txt", ios::app);
+	fout << "name,id,username,password";
+	while (pCur != nullptr) {
+		fout << pCur->name << "," << pCur->id << "," << pCur->username << "," << pCur->password;
+		pCur = pCur->pNext;
+	}
+
+	fout.close();
+}
 void addStudentNode(studentNode*& head, studentNode*& end, studentNode* pItem);
 void addStaffNode(staffNode*& head, staffNode*& end, staffNode* pItem);
