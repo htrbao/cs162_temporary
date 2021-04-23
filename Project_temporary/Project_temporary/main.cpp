63213582e910ed33a9686f4c79db282990eb2443
heeyoung-choi <75930620+heeyoung-choi@users.noreply.github.com>
2021-04-23 15:50:21 +0700

    Add files via upload

--- Project_temporary/Project_temporary/main.cpp
+++ Project_temporary/Project_temporary/main.cpp
@@ -2,31 +2,33 @@
 #include "interface.h"
 #include "staffInterface.h"
 #include "studentInterface.h"
+#include "login.h"
 
 HANDLE wHnd;
 HANDLE rHnd;
 
 int main()
-{
-	//setting all
-	wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
-	rHnd = GetStdHandle(STD_INPUT_HANDLE);
-	SMALL_RECT windowSize = { 0, 0, 200, 40 };
-	COORD bufferSize = { 200, 9001 };
-	SetConsoleScreenBufferSize(wHnd, bufferSize);
-	SetConsoleWindowInfo(wHnd, TRUE, &windowSize);
-	updateStudent("STUDENT.csv");
-	updateStaff("STAFF.csv");
-	//end
-	//displayStudentList(findClass("20CTT2", 0));
+{	
 
-	studentNode* a = new studentNode;
-	a->name = "NHÂN";
+	////setting all
+	//wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
+	//rHnd = GetStdHandle(STD_INPUT_HANDLE);
+	//SMALL_RECT windowSize = { 0, 0, 200, 40 };
+	//COORD bufferSize = { 200, 9001 };
+	//SetConsoleScreenBufferSize(wHnd, bufferSize);
+	//SetConsoleWindowInfo(wHnd, TRUE, &windowSize);
+	//updateStudent("STUDENT.csv");
+	//updateStaff("STAFF.csv");
+	////end
+	////displayStudentList(findClass("20CTT2", 0));
+	//staffMenu(headStaff);
 
-	studentMenu(a);
+
+	loginScreen();
+
 
 	cleanUpClass();
 	cleanUpStaff();
-
+	
 	return 0;
 }
